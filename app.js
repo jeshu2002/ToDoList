2b4ae2e3b8d60f797ce13498752127f0f2933db7
jeshu2002 <71264878+jeshu2002@users.noreply.github.com>
2022-08-02 22:26:12 +0530

    Updating files

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.static("public"))
 let values = ["car", "bike", "audi"];
 let worklist = [];
+let weekendlist = [];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -16,16 +17,25 @@ app.get("/work", function(req, res) {
   res.render("list", {listTitle: "Work List("+date.day+")",list: worklist})
 })
 
+app.get("/weekend", function(req, res) {
+  res.render("list", {listTitle: "Weekend plans("+date.day+")",list: weekendlist})
+})
+
 app.get("/about", function(req, res) {
   res.render("about")
 })
 
 app.post("/", function(req, res) {
   let value = req.body.input;
-  if (req.body.button === "Work") {
+  let homevalue =date.day+",";
+  if (req.body.button === "Work" && value !=="") {
     worklist.push(value);
     res.redirect("/work");
-  } else {
+  }
+  else if(req.body.button === "Weekend" && value !==""){
+    weekendlist.push(value);
+    res.redirect("/Weekend");
+  }else if(req.body.button === homevalue && value !==""){
     values.push(value);
     res.redirect("/");
   }
